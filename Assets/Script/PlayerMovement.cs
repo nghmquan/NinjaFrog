e5119b2f249f6c71abedea8a6c42748dee5d175c
Nguyễn Hoàng Minh Quân <109788332+nghmquan@users.noreply.github.com>
2024-03-19 22:15:30 +0700

    Start Game

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Numerics;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float moveSpeed = 1f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            rb.transform.position = new UnityEngine.Vector2(moveSpeed * Time.deltaTime, rb.position.y);
+        }
+
+        Debug.Log("rb: " + rb);
+    }
+
+}
---
+++ Assets/Script/SpikedBallSpawn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikedBallSpawn : MonoBehaviour
+{
+    public GameObject objectPrefab;
+    public float spawnRate = 2f;
+    public float spawnDelay = 1f;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnObject", spawnDelay, spawnRate);
+    }
+
+    void SpawnObject()
+    {
+        float randomY = Random.Range(-2f, 2f); // Vị trí ngẫu nhiên theo trục x
+        Vector3 spawnPosition = new Vector3(transform.position.x, randomY, 0f);
+        Instantiate(objectPrefab, spawnPosition, Quaternion.identity);
+    }
+}
